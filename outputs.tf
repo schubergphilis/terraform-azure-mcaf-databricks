34b6db4d31961db0cb619200ee58d74153b6e1ac
atmahussien <ahussien@schubergphilis.com>
2025-02-11 20:38:31 +0100

    Removed identity output

--- outputs.tf
+++ outputs.tf
@@ -13,11 +13,6 @@ output "databricks_workspace_name" {
   description = "The name of the Databricks workspace."
 }
 
-output "databricks_app_object_id" {
-  value       = try(azurerm_databricks_workspace.this.identity.0.principal_id, null)
-  description = "The Object ID of the Databricks managed identity."
-}
-
 output "metastore_id" {
   value       = var.metastore_id
   description = "The metastore ID assigned to the Databricks workspace."
