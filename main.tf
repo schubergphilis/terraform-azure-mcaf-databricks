4984866acb548e68cacc3aea432d221b4adf5242
atmahussien <ahussien@schubergphilis.com>
2025-02-12 16:26:19 +0100

    removed extra duplicate outputs

--- main.tf
+++ main.tf
@@ -69,14 +69,3 @@ resource "azurerm_databricks_workspace" "this" {
     ignore_changes = [tags]
   }
 }
-
-# Outputs
-output "private_subnet_nsg_id" {
-  value       = azurerm_network_security_group.private.id
-  description = "The ID of the Network Security Group (NSG) associated with the private subnet."
-}
-
-output "public_subnet_nsg_id" {
-  value       = azurerm_network_security_group.public.id
-  description = "The ID of the Network Security Group (NSG) associated with the public subnet."
-}
